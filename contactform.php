83b48555ba1973b1aa742a63f12353fa9df35b30
Bryan Jerger <Bjerger1470@gmail.com>
2020-04-27 18:50:32 -0500

    contactform.php created

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+    
+    $mailTo = "contactme@bryanmjerger.net";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an Email from ".$name ".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("location: index.html?mailsent");
+}
