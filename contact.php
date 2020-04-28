ab606f3ebe2932eef78d054e133e9d0c757cbfdf
Bryan Jerger <Bjerger1470@gmail.com>
2020-04-27 20:57:34 -0500

    added validation

--- contact.php
+++ contact.php
@@ -1,12 +1,17 @@
+<?php session_start(); ?>
+<?php require("botdetect.php"); ?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel ="stylesheet" type = "text/css" href="assets/css/style.css">
+    <link type="text/css" rel="Stylesheet" 
+    href="<?php echo CaptchaUrls::LayoutStylesheetUrl() ?>" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+    
 
     <title>Bryan's Portfolio</title>
 </head>
@@ -62,6 +67,17 @@
 </br>
     <textarea name="message" placeholder= "Message"></textarea>
 </br>
+    <label for="CaptchaCode">Retype the characters from the picture:
+    </label>
+
+    <?php // Adding BotDetect Captcha to the page 
+    $ExampleCaptcha = new Captcha("ExampleCaptcha");
+    $ExampleCaptcha->UserInputID = "CaptchaCode";
+    echo $ExampleCaptcha->Html(); 
+    ?>
+
+<input name="CaptchaCode" id="CaptchaCode" type="text" />
+
     <button class="btn btn-primary" name="submit" type="submit">Send</button>
     
 </form>
--- contactform.php
+++ contactform.php
@@ -1,4 +1,7 @@
 <?php
+session_start(); 
+require("botdetect.php"); 
+
 
 if (isset($_POST['submit'])){
     $name = $_POST['name'];
@@ -9,6 +12,17 @@ if (isset($_POST['submit'])){
     $mailTo = "contactme@bryanmjerger.net";
     $headers = "From: ".$mailFrom;
     $txt = "You have recieved an Email from ".$name.".\n\n".$message;
+
+    // Captcha validation 
+    $ExampleCaptcha = new Captcha("ExampleCaptcha");
+    $ExampleCaptcha->UserInputID = "CaptchaCode";
+    $isHuman = $ExampleCaptcha->Validate();
+
+    if (!$isHuman) { 
+    // Captcha validation failed, redirect back to form page
+    header("Location: contact.php?captchaValid=false");
+    exit;
+}
 
     mail($mailTo, $subject, $txt, $headers);
     header("location: contact.php?mailsent");
